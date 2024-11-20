eb1c5e04b53e7afb2726c14bdc45f7901f555016
Gavesh Saparamadu <gaveshasaparamadu06@gmail.com>
2024-11-20 21:12:09 +0530

    Added blurred contour to Homepage

--- src/routes/(portfolio)/+layout.svelte
+++ src/routes/(portfolio)/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../../app.css';
 	import Header from './comp/Header.svelte';
 	import Navigation from './comp/Navigation.svelte';
+	import Canvas from './comp/Canvas.svelte';
 	import { fade } from 'svelte/transition';
 	let { children, data } = $props();
 	let pathname = $derived(data);
@@ -12,12 +13,15 @@
 </svelte:head>
 
 <div class="py-10 px-8 sm:py-16 sm:px-14 flex flex-col min-h-screen gap-6">
-	<Header />
-	<Navigation />
-	<hr class="opacity-35" />
-	{#key pathname}
-		<div in:fade={{ duration: 450, delay: 0 }} out:fade={{ duration: 0, delay: 0 }}>
-			{@render children()}
-		</div>
-	{/key}
+	<Canvas />
+	<div class="z-20 flex flex-col gap-6">
+		<Header />
+		<Navigation />
+		<hr class="opacity-35" />
+		{#key pathname}
+			<div in:fade={{ duration: 450, delay: 0 }} out:fade={{ duration: 0, delay: 0 }}>
+				{@render children()}
+			</div>
+		{/key}
+	</div>
 </div>
---
+++ src/routes/(portfolio)/comp/Canvas.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Contour from '$lib/assets/contours.svg';
+</script>
+
+<div
+	class="absolute select-none z-10 pointer-events-none overflow-hidden left-0 top-0 w-full h-screen debug"
+>
+	<img src={Contour} class="blur-lg contour w-full h-full opacity-55" alt="" />
+</div>
+
